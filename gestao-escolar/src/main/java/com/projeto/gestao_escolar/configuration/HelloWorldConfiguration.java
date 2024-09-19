921a0932a01212cf4b37fa370aa2819bf90992f7
Guga2111 <luisgosampaio@gmail.com>
2024-09-19 17:32:07 -0300

    adição de dependencia 'thymeleaf' e criação de html teste

--- gestao-escolar/src/main/java/com/projeto/gestao_escolar/configuration/HelloWorldConfiguration.java
+++ gestao-escolar/src/main/java/com/projeto/gestao_escolar/configuration/HelloWorldConfiguration.java
@@ -6,5 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HelloWorldConfiguration {
     @Bean
-    public String helloWorld() {return "Hello World!";}
+    public String helloWorld() {
+        return "Hello World!";
+    }
 }
